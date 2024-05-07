dd501501aae158c8af87a92e78a5c542069bc0c4
P2724033 <P2724033@my365.dmu.ac.uk>
2024-05-07 15:42:10 +0100

    Add new web form stuff.

--- AdminSystem/OrderDataEntry.aspx.cs
+++ AdminSystem/OrderDataEntry.aspx.cs
@@ -4,10 +4,28 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        // Create a new instance of clsOrder
+        clsOrder AnOrder = new clsOrder();
+        // Capture the Customer Note
+        AnOrder.CustomerNote = txtCustomerNote.Text;
+        // Store the note in the session object.
+        Session["AnOrder"] = AnOrder;
+        // Navigate to the view page
+        Response.Redirect("OrderViewer.aspx");
+    }
+
+    protected void CheckBox1_CheckedChanged(object sender, EventArgs e)
     {
 
     }
--- ClassLibrary/clsOrder.cs
+++ ClassLibrary/clsOrder.cs
@@ -6,7 +6,7 @@ namespace ClassLibrary
     {
         public bool IsPaid { get; set; }
         public DateTime DateOrderPlaced { get; set; }
-        public DateTime DateOrderPaid { get; set; }
+        public string DeliveryType { get; set; }
         public int OrderPrice { get; set; }
         public string StaffNote { get; set; }
         public string CustomerNote { get; set; }
