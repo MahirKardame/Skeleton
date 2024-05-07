2af74b9cd3136f0fb84910febcf3ce0e1f9111bd
p2724513 <p2724513@my365.dmu.ac.uk>
2024-05-07 14:51:16 +0100

    created datatype for staff table

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -1,9 +1,19 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStaff
     {
         public clsStaff()
         {
         }
+
+        public bool IsAdmin { get; set; }
+        public DateTime dateJoined { get; set; }
+        public int staffId { get; set; }
+        public string staffName { get; set; }
+        public string jobTitle { get; set; }
+        public string staffEmail { get; set; }
+        public string staffAddress { get; set; }
     }
 }
