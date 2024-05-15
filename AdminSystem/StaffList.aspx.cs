68ae209a0fb4e0c9178d33a376769364b0e4b691
p2724513 <p2724513@my365.dmu.ac.uk>
2024-05-15 10:53:57 +0100

    created the colection class

--- AdminSystem/StaffList.aspx.cs
+++ AdminSystem/StaffList.aspx.cs
@@ -4,11 +4,30 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_List : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        //if this is the first time the page is displayed
+        if (IsPostBack == false)
+        {
+            //update the list box
+            DisplayStaffMembers();
+        }
+    }
+    void DisplayStaffMembers()
+    {
+        //create an instance of the staff collection
+        clsStaffCollection StaffMembers = new clsStaffCollection();
+        //set the data source to the list of staff members in the collection
+        lstStafflist.DataSource = StaffMembers.StaffList;
+        //set the name of the primary key
+        lstStafflist.DataValueField = "StaffId";
+        //set the data field to display
+        lstStafflist.DataTextField = "staffName";
+        //bind the data to the list
+        lstStafflist.DataBind();
     }
 }
---
+++ Testing5/tstStaffCollection.cs
@@ -0,0 +1,92 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Testing5
+{
+    
+    [TestClass]
+    public class tstStaffCollection
+    {
+        [TestMethod]
+        public void InstanceOk()
+        {
+            //create an instance of the class
+            clsStaffCollection AllStaff = new clsStaffCollection();
+            //test to see that it exists
+            Assert.IsNotNull(AllStaff);
+        }
+        [TestMethod]
+        public void StaffListOk() 
+        {
+            //create an instance of the class
+            clsStaffCollection AllStaff = new clsStaffCollection();
+            //create some test data to assign to the property, in this case a list
+            List<clsStaff> TestList = new List<clsStaff>();
+            //create the item of test data
+            clsStaff TestItem = new clsStaff();
+            //set the properties
+            TestItem.StaffId = 1;
+            TestItem.StaffName = "bob";
+            TestItem.JobTitle = "admin";
+            TestItem.StaffEmail = "bob@work.com";
+            TestItem.StaffAddress = "1 main street";
+            TestItem.DateJoined = DateTime.Now;
+            TestItem.IsAdmin = true;
+            //add the item to the test list
+            TestList.Add(TestItem);
+            //assign the data to the property
+            AllStaff.StaffList = TestList;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllStaff.StaffList, TestList);
+
+        }
+        [TestMethod]
+        public void ThisStaffMemberPropertyOK()
+        {
+            //create an instance of the class
+            clsStaffCollection AllStaff = new clsStaffCollection();
+            //create some test data to assign to the property
+            clsStaff TestItem = new clsStaff();
+            //set the properties
+            TestItem.StaffId = 1;
+            TestItem.StaffName = "bob";
+            TestItem.JobTitle = "admin";
+            TestItem.StaffEmail = "bob@work.com";
+            TestItem.StaffAddress = "1 main street";
+            TestItem.DateJoined = DateTime.Now;
+            TestItem.IsAdmin = true;
+            //assign the data to the property
+            AllStaff.ThisStaffMember = TestItem;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllStaff.ThisStaffMember, TestItem);
+
+        }
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            //create an instance of the class
+            clsStaffCollection AllStaff = new clsStaffCollection();
+            //create some test data to assign to the property, in this case a list
+            List<clsStaff> TestList = new List<clsStaff>();
+            //create the item of test data
+            clsStaff TestItem = new clsStaff();
+            //set the properties
+            TestItem.StaffId = 1;
+            TestItem.StaffName = "bob";
+            TestItem.JobTitle = "admin";
+            TestItem.StaffEmail = "bob@work.com";
+            TestItem.StaffAddress = "1 main street";
+            TestItem.DateJoined = DateTime.Now;
+            TestItem.IsAdmin = true;
+            //add the item to the test list
+            TestList.Add(TestItem);
+            //assign the data to the property
+            AllStaff.StaffList = TestList;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllStaff.Count, TestList.Count);  
+        }
+    }
+}
